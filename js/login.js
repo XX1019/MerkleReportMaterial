d12c6c25e40b09e4520605c4efdb8733a30bd4ac
XX1019 <waxx_1019@163.com>
2015-11-18 13:21:24 +0800

    login and sign up

---
+++ js/login.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	AV.initialize('LxpfQik9wwJivKv9dygidXCM', 'uHVT6193PNbwx6Wf5NnExCF5');
+});
+
+function login() {
+
+
+	var username = document.getElementById("icon_name");
+	var pwd = document.getElementById("icon_pwd");
+
+	var myname = username.value;
+	var mypass = pwd.value;
+	console.log(myname);
+	console.log(mypass);
+
+	AV.User.logIn(myname, mypass, {
+		success: function(user) {
+			// 成功了，现在可以做其他事情了.
+			console.log("successed");
+
+			window.location.href = "totalfunction.html";
+		},
+		error: function(user, error) {
+			// 失败了.
+			alert("密码或用户名不正确！");
+		}
+	});
+
+
+}
---
+++ js/signup.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	AV.initialize('LxpfQik9wwJivKv9dygidXCM', 'uHVT6193PNbwx6Wf5NnExCF5');
+});
+
+
+function cancel() {
+	window.location.href = "index.html";
+}
+
+function register() {
+	//得到注册的用户名 密码
+	var username = document.getElementById("icon_name");
+	var pwd = document.getElementById("icon_pwd");
+	var confirm = document.getElementById("confirm_pwd");
+	console.log(username.value);
+	console.log(pwd.value);
+	console.log(confirm.value);
+
+if(pwd.value!=confirm.value){
+	alert("Password is not consistent");
+}else{
+	var user = new AV.User();
+	user.set("username", username.value);
+	user.set("password", pwd.value);
+
+
+	user.signUp(null, {
+		success: function(user) {
+			// 注册成功，可以使用了.
+			alert("注册成功！")
+			window.location.href = "index.html";
+		},
+		error: function(user, error) {
+			// 失败了
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+
+}
+
+	
+}
