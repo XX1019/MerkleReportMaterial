b2a880814a506a7942f7bc7d2f259c1142e74aec
XX1019 <waxx_1019@163.com>
2015-11-19 11:05:58 +0800

    start to write function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	AV.initialize('LxpfQik9wwJivKv9dygidXCM', 'uHVT6193PNbwx6Wf5NnExCF5');
 	var user_current = null;
 user_current = AV.User.current();
-document.getElementById("user").innerHTML = user_current.get('username');
+document.getElementById("user").innerHTML = "Hi~ "+user_current.get('username')+", Welcome!";
 });
 
